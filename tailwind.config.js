a5c73845e81ae229bb34cb18b07ede7057c119a7
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2024-03-16 01:06:27 +0530

    :sparkles: add color code snippet for course

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brand-blue": "#0d6efd",
+        "brand-red": "#dc3545",
+        "brand-green": "#198754",
+        "brand-yellow": "#ffc107",
+        "brand-gray": "#6c757d",
+        "brand-light": "#f8f9fa",
+        "brand-dark": "#212529",
+      },
+    },
   },
   plugins: [],
 };
