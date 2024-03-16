24e1e9db338a8c8f730cbba1a6858f016253daaa
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2024-03-16 14:01:22 +0530

    :sparkles: add typography related code snippet

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "leage-script": ["League Script", "cursive"],
+        unlock: ["Unlock", "serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+      fontSize: {
+        "mega-font": "40px",
+        "new-xl": [
+          "4.5rem",
+          {
+            lineHeight: "1.2",
+          },
+        ],
+      },
+    },
   },
   plugins: [],
 };
